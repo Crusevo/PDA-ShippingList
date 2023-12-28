413a0f4a65d8a516d33948d7a778290f4946694d
Crusevo <mateuszandrzejprog@gmail.com>
2023-12-28 18:43:57 +0100

    Add some features

--- src/main/java/com/example/PDA/ShippingList/PageController/HomePageController.java
+++ src/main/java/com/example/PDA/ShippingList/PageController/HomePageController.java
@@ -1,12 +1,29 @@
 package com.example.PDA.ShippingList.PageController;
 
+import com.example.PDA.ShippingList.Model.Project;
+import com.example.PDA.ShippingList.Repository.ProjectRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class HomePageController {
+
+    @Autowired
+    ProjectRepository projectRepository;
+
+
     @GetMapping("/homePage/")
-    public String getHomePage() {
+    public String getHomePage(Model model) {
+
+        List<Project> allProjects = projectRepository.findAll();
+
+        model.addAttribute("allProjects", allProjects);
+
+
         return "homePage";
     }
 
